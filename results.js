79ab32f17e559981812a8b74e604cac455d15be2
harishmukkapati <54966140+harishmukkapati@users.noreply.github.com>
2023-10-19 15:51:53 -0400

    Update results.js

--- results.js
+++ results.js
@@ -1,30 +1,9 @@
 // Stores the information from classes.json into a variable
 let api = getData();
 
-// Assigning the background color of each card based on the college name
-let collegeColor = new Map();
-collegeColor.set("The Ohio State University", "#BB0000");
-collegeColor.set("Georgia Institure of Technology", "#B3A369");
-collegeColor.set("Massechusetts Institute of Technology", "#8a8b8c");
-collegeColor.set("Stanford University", "#B83A4B");
-collegeColor.set("University of Michigan", "#00274C");
-collegeColor.set("University of California, Berkeley", "#003262");
-
-
 // Get the search string from sessionStorage
 let searchString = sessionStorage.getItem('searchString');
 console.log("Search String:", searchString);
-
-// Get the JSON data from classes.json
-// Populate page with cards which contain the searchString either in the university name, course code, or alternate name
-    // Look to Pokémon site for what cards may look like
-    // Make card taller slightly, show the unviersity's SVG logo, and the course's alternate name.
-// When clicking on any card except OSU's software 1, display a modal which states "In development!"
-// When clicking on OSU's software 1 card, it should link to the page where user can chat with the llm finetuned for that specific course
-
-
-
-
 
 // Get the data from classes.json and store it in api
 async function getData() {
@@ -33,52 +12,105 @@ async function getData() {
     return data;
 }
 
-// Go one level down in api to access the universities, and write a for each loop to traverse it
-// For each university, go one level down to access the courses, and write a for each loop to traverse it
-// For each course, check if the search string is contained within the university name, course code, or alternate name
-    // If it is, then create a card for that course and append it to the page
 // Populate the page with cards based on the search string
-async function populatePage() {
-    let api = await getData();
-    let universityArray = api.universities;
-    universityArray.forEach(university => {
-        let courseArray = university.classes;
-        courseArray.forEach(course => {
-            if (university.name.includes(searchString) || course.course_code.includes(searchString) || course.alternate_name.includes(searchString)) {
-                createCard(university, course);
-            }
+function createCard(university, course) {
+    // Create the card container
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    // Create and set the SVG icon for the university
+    const universityLogo = document.createElement('img');
+    universityLogo.className = 'card-image';
+    universityLogo.src = university.logo;  // Assuming the SVG URL is stored under 'logo' key in classes.JSON
+    universityLogo.alt = university.name;
+    card.appendChild(universityLogo);
+
+    // Create and set the university name
+    const universityName = document.createElement('div');
+    universityName.className = 'card-title';
+    universityName.textContent = university.name;
+    card.appendChild(universityName);
+
+    // Create and set the course code
+    const courseCode = document.createElement('div');
+    courseCode.className = 'card-text';
+    courseCode.textContent = course.course_code;
+    card.appendChild(courseCode);
+
+    // Create and set the alternate name
+    const alternateName = document.createElement('div');
+    alternateName.className = 'card-text';
+    alternateName.textContent = course.alternate_name;
+    card.appendChild(alternateName);
+
+    // Only go to site for LLM if course seleected is Software 1
+    if (university.name === "The Ohio State University" && course.course_code === "CSE 2221") {
+        card.addEventListener('click', function() {
+            window.location.href = "https://www.google.com";
+        });
+    } else {
+        // Show modal button event listener
+        card.addEventListener('click', function() {
+            showModal();
+        });
+    }
+
+    // Assigns colors to each university
+    const collegeColor = {
+        "The Ohio State University": "#A7B1B7",
+        "Georgia Institute of Technology": "#FDB515",
+        "Massachusetts Institute of Technology": "#000000",
+        "Stanford University": "#B83A4B",
+        "University of Michigan": "#00274C",
+        "University of California, Berkeley": "#3B7EA1",
+        "University of Illinois at Urbana-Champaign": "#FF5F05",
+    };
+
+    if (collegeColor[university.name]) {
+        card.style.backgroundColor = collegeColor[university.name];
+    }
+
+    // Append the card to the card container in the HTML
+    document.querySelector('.card-container').appendChild(card);
+}
+
+function populatePage() {
+    fetch('classes.json')
+    .then(response => response.json())
+    .then(data => {
+        let searchString = sessionStorage.getItem('searchString').toLowerCase();
+
+        data.universities.forEach(university => {
+            let universityNameMatch = university.name.toLowerCase().includes(searchString);
+
+            university.classes.forEach(course => {
+                let courseCodeMatch = course.course_code.toLowerCase().includes(searchString);
+                let alternateNameMatch = course.alternate_name.toLowerCase().includes(searchString);
+
+                if (universityNameMatch || courseCodeMatch || alternateNameMatch) {
+                    createCard(university, course);
+                }
+            });
         });
     });
 }
 
-// Create a card for a course and append it to the page
-function createCard(university, course) {
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.style.backgroundColor = collegeColor.get(university.name);
-    let cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-    let cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = course.course_code; // Changed from course.name to course.course_code
-    let cardText = document.createElement('p');
-    cardText.classList.add('card-text');
-    cardText.textContent = course.alternate_name;
-    let cardButton = document.createElement('a');
-    cardButton.classList.add('btn');
-    cardButton.classList.add('btn-primary');
-    cardButton.textContent = 'Go to chat';
-    cardButton.href = 'chat.html';
-    cardButton.addEventListener('click', () => {
-        sessionStorage.setItem('course', JSON.stringify(course));
-    });
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    cardBody.appendChild(cardButton);
-    card.appendChild(cardBody);
-    document.querySelector('.card-container').appendChild(card);
-}
-
-// Call the populatePage function to display the cards
+// Call the populatePage function when the page loads
 populatePage();
 
+
+function showModal() {
+    const modal = document.getElementById('courseModal');
+    modal.style.display = "block";
+
+    const closeBtn = document.querySelector('.close');
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+}
